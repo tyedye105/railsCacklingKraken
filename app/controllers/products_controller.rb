d8dcf70a2535cd931d4ff1d682e23275d575deff
Marty Kovach <tyedye105@yahoo.com>
2017-02-17 00:11:37 -0800

    create function for admin

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,13 +6,28 @@ class ProductsController < ApplicationController
   def show
   end
   def new
+    @user = current_user
+    @product = @user.products.new
   end
   def create
+    @user = current_user
+    @product = @user.products.new(product_params)
+      if @user.save
+        flash[:notice] = "You have added new merchandise to be sold!"
+        redirect_to products_path
+      else
+        render :new
+      end
   end
   def edit
   end
   def update
   end
   def destroy
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :description, :found_in, :going_price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :products, :only => [:show, :index]
   resources :user do
     resources :products
+
   end
+  resources :products
 end
