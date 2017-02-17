f45e89c9714ce46c55ed84e5d69b790a188dfada
Marty Kovach <tyedye105@yahoo.com>
2017-02-16 22:44:55 -0800

    add errors partial

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,18 @@
 class ProductsController < ApplicationController
+  def index
+    @user = current_user
+    @products = Product.all
+  end
+  def show
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
 end
