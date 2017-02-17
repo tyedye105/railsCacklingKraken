083d2d73fe0cd10f310c0baa522bbcf7da507a42
Marty Kovach <tyedye105@yahoo.com>
2017-02-16 21:31:09 -0800

    initial commit. Gems for bootstrap, simplecov, factorygirl, and devise installed and set up.  Create user model. Create home#index view.

---
+++ db/migrate/20170217051524_devise_create_users.rb
@@ -0,0 +1,23 @@
+class DeviseCreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :user_name
+      t.string :name
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
+  end
+end
